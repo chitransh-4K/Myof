e408733438f865fe2a1c21a3925d4a62a8241d35
unknown <ddrelectroid@gmail.com>
2025-02-08 13:18:07 +0530

    new structure

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const app = express()
 app.set("view engine","ejs")
 
 app.use(express.static("public"))
-app.get("/",(req,res)=>{
-    res.render("index")
-})
-
 app.get("/web-forge",(req,res)=>{
     res.render("web")
 })
